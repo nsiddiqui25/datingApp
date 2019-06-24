1d7700a7979263b7b0caf42704883439355b779c
nsiddiqui25 <n.siddiqui25@gmail.com>
2019-06-24 14:10:18 -0500

    injective angular auth service to nav component

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -8,13 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log(this.model);
+    // console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Login success');
+    }, error => {
+      console.log('Login failure');
+    });
   }
 
 }
