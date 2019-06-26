2582303dbe2dee56a61610008993457e890a2d84
nsiddiqui25 <n.siddiqui25@gmail.com>
2019-06-26 10:00:53 -0500

    using routing in our components

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { iif } from 'rxjs';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -24,6 +25,8 @@ export class NavComponent implements OnInit {
     }, error => {
       // console.log(error);
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -35,6 +38,7 @@ export class NavComponent implements OnInit {
     localStorage.removeItem('token');
     // console.log('logged out');
     this.alertify.message('You have been logged out!');
+    this.router.navigate(['/home']);
   }
 
 }
