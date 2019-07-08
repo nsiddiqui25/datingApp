f9e55e99ce290f70ae1dd8e781e671fa61e9f715
nsiddiqui25 <n.siddiqui25@gmail.com>
2019-07-08 14:27:38 -0500

    use the array filer() method to provide instant feedback in SPA

--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -17,6 +17,7 @@ export class PhotoEditorComponent implements OnInit {
   uploader: FileUploader;
   hasBaseDropZoneOver = false;
   baseUrl = environment.apiUrl;
+  currentMain: Photo;
 
   constructor(private authService: AuthService, private userServier: UserService, private alertify: AlertifyService) { }
 
@@ -60,7 +61,10 @@ export class PhotoEditorComponent implements OnInit {
 
   setMainPhoto(photo: Photo) {
     this.userServier.setMainPhoto(this.authService.decodedToken.nameid, photo.id).subscribe(() => {
-      console.log('Successfully set to main.');
+      // console.log('Successfully set to main.');
+      this.currentMain = this.photos.filter(p => p.isMain === true)[0];
+      this.currentMain.isMain = false;
+      photo.isMain = true;
     }, error => {
       this.alertify.error(error);
     });
