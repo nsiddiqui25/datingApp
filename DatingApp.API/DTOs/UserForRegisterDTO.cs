ba5a5a762d11702ee2e1638c60ec39e8f3233f63
nsiddiqui25 <n.siddiqui25@gmail.com>
2019-07-10 14:13:58 -0500

    update Register method in the API

--- DatingApp.API/DTOs/UserForRegisterDTO.cs
+++ DatingApp.API/DTOs/UserForRegisterDTO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatingApp.API.DTOs
@@ -13,5 +14,27 @@ namespace DatingApp.API.DTOs
         [Required]
         [StringLength(12, MinimumLength = 8, ErrorMessage = "Your password must be between 8 to 12 characters")]
         public string Password { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string KnownAs { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public UserForRegisterDTO()
+        {
+            Created = DateTime.Now;
+            LastActive = DateTime.Now;
+        }
     }
 }
